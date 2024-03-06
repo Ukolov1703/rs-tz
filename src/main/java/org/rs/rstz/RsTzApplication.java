8c2fa1815b60757c5c7e3df38a4be486ad783300
Ukolov1703 <ukollov@inbox.ru>
2024-03-06 19:20:07 +0700

    Initial commit

---
+++ src/main/java/org/rs/rstz/RsTzApplication.java
@@ -0,0 +1,13 @@
+package org.rs.rstz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsTzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsTzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rs/rstz/RsTzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rs.rstz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsTzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
